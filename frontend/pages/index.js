043761bb6716d74e74dd4cef9e31e3b640977c92
ShahabAthar25 <shahabathar25@gmail.com>
2021-11-30 12:17:18 +0500

    Made header responsive

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,19 @@
 import Header from "../components/Header";
+import { Button } from "@mui/material";
+import { DotsVerticalIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <h1>Hello World</h1>
+      <div className="max-w-6xl mx-auto">
+        <div className="py-6 flex items-center justify-between">
+          <h1 className="text-gray-600 text-lg">Start a new document</h1>
+          <Button className="rounded-full">
+            <DotsVerticalIcon className="h-6 text-gray-600" />
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
