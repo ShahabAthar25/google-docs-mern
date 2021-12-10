f5c71a422c54e8d320f1e3e0ac603984681df8e7
ShahabAthar25 <shahabathar25@gmail.com>
2021-12-10 19:58:10 +0500

    Cleaned Up

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,10 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {
-      maxWidth: {
-        44: "11rem",
-        xxs: "12rem",
-        xs: "15rem",
-        md: "20rem",
-      },
-      height: {
-        "5/7": "90%",
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
-};
+}
